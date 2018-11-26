7cd006aace6c9e60f01ec4225a956e47e18eda27
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-26 21:16:53 +0100

    Genereting unique id for each item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid'
 import Projects from './components/Projects'
 import AddProject from './components/AddProject'
 import './App.css';
@@ -14,14 +15,17 @@ class App extends Component {
   componentWillMount() {
     this.setState({
       projects: [{
+        id: uuid.v4(),
         title: 'Business Website',
         category: 'Web Design'
       },
       {
+        id: uuid.v4(),
         title: 'Social App',
         category: 'Mobile Development'
       },
       {
+        id: uuid.v4(),
         title: 'E-Commerce Shopping Cart',
         category: 'Web Development'
       }]

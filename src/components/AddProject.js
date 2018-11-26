712aab133ba140c49b4c89212b3723fdb915d5e0
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-26 21:34:46 +0100

    Setting propTypes

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -59,4 +59,9 @@ class AddProject extends Component {
     }
 }
 
+AddProject.propTypes = {
+    categories: React.PropTypes.array,
+    addProject: React.PropTypes.func
+}
+
 export default AddProject;

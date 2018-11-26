72fd4daa6d0fc26f831cb223cf1fb4b702417cf8
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-26 20:48:57 +0100

    Render each project item from Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
 
@@ -7,12 +8,18 @@ class Projects extends Component {
 
         if (this.props.projects) {
             projectItems = this.props.projects.map(project => {
-                console.log(project)
+                //console.log(project)
+                return (
+                    <ProjectItem
+                        key={project.title}
+                        project={project}
+                    />
+                )
             })
         }
         return (
             <div className="Projects">
-                My Projects
+                {projectItems}
             </div>
         );
     }

e18621bc0406cfba7e70d78f7e6871175cdaf980
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-26 20:51:47 +0100

    Passing items to state with lifecycle method

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      projects: [
-        {
-          title: 'Business Website',
-          category: 'Web Design'
-        },
-        {
-          title: 'Social App',
-          category: 'Mobile Development'
-        },
-        {
-          title: 'E-Commerce Shopping Cart',
-          category: 'Web Development'
-        }
-      ]
+      projects: []
     }
   }
+
+  componentWillMount() {
+    this.setState({
+      projects: [{
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        title: 'E-Commerce Shopping Cart',
+        category: 'Web Development'
+      }]
+    })
+  }
+
   render() {
     return (
       <div className="App">

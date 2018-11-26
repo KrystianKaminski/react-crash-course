c39e6292a968ad715ef4b9e4f98f76751b148eb2
KrystianKaminski <krystiankaminski07@gmail.com>
2018-11-26 21:07:54 +0100

    Handling add project method

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -22,7 +22,7 @@ class AddProject extends Component {
                     category: this.refs.category.value
                 }
             }, function () {
-                console.log(this.state)
+                this.props.addProject(this.state.newProject)
             })
         }
         e.preventDefault()
